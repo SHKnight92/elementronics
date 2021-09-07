4e710fe4b7f6830bba944c32aba0719d2a874ed1
SHKnight92 <saddam.hossain.knight@gmail.com>
2021-09-07 22:36:16 +0600

    first commit

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,1 +1,1 @@
-self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/shop/checkout":["static\u002Fchunks\u002Fpages\u002Fshop\u002Fcheckout.js"],"/shop/checkout-complete":["static\u002Fchunks\u002Fpages\u002Fshop\u002Fcheckout-complete.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
